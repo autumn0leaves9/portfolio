5773a183afd5c99e34e1997d6124eb6a3ddde834
autumn0leaves9 <72963877+autumn0leaves9@users.noreply.github.com>
2020-10-22 00:37:56 +0900

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BANBOO EFFECTS</title>
+    <link rel="stylesheet" href="SoundCreateCSS.css" >
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body link="#fff" vlink="#fff" alink="blue">
+  
+    <header>
+      <div class="logo"><a href="SoundCreate.html" style="text-decoration:none;">BANBOO EFFECTS</a></div>
+      <ul class="headerli">
+        <li><a href="effect.html">Effects</a></li>
+        <li><a href="youtube.html">YouTube</a></li>
+        <li><a href="contact.php">Contact</a></li>
+		<li><a href="https://twitter.com/Under_Bamboo_TL?s=20">Twitter</a></li>
+		
+      </ul>
+    </header>
+	
+	<div class="main">
+	
+	  
+	  <div class="contact-form">
+        <h3 class="contact-title">お問い合わせ</h3>
+		
+		<form action="sent.php" method="post">
+		
+		<p>名前</p>
+		<input name="name" type="text" placeholder="(例)xxx">
+		
+        <p>メールアドレス</p>
+       <input name="mail" type="text" placeholder="(例)xxx@example.com">
+        
+        <p>お問い合わせ内容</p>
+        <textarea name="body" placeholder="(例)お問い合わせ内容詳細"></textarea>
+        
+        <p>※必ず全てご入力ください</p>
+        <input class="contact-submit" type="submit" value="送信">
+        
+      </div>
+	  
+    </div>
+	
+	<footer>
+     <div class="footer-logo">松竹</div>
+      
+    </footer>
+	
+  </body>
+</html>
